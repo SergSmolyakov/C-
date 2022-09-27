24c6b535c001abe6e25838834e797d87583c57bd
SergSmolyakov <sergey@web-development1.ru>
2022-09-27 10:40:56 +0300

    Add Task25

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,13 +1,14 @@
 ﻿System.Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
-int num1 = num;
-int reversive = 0;
+int num1 = num; 
+int reversive = 0; 
 while (num1 > 0)
 {
-    int r = num1 % 10;
+    int r = num1 % 10; 
     reversive = reversive * 10 + r;
     num1 = num1 / 10;
 }
+
 if (reversive == num)
 {
     System.Console.WriteLine($"Число {num} => да, является палиндромом");
---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int DegreeNum(int num1, int num2){
+  int result = 1;
+  for(int i = 1; i <= num2; i++){
+    result = result * num1;
+  }
+      return result;
+}
+
+  Console.Write("Введите число которое нужно возвести в степень: ");
+  int num1 = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите степень: ");
+  int num2 = Convert.ToInt32(Console.ReadLine());
+
+  int degree = DegreeNum(num1, num2);
+  Console.WriteLine($"{num1} в степени {num2} будет равняться {degree}. ");
